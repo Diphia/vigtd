96dbcd9d22b9d3828e8376c6c7475ed2ab9a6c7e
Diphia <zqygodness@gmail.com>
2020-03-20 12:13:49 +0800

    rewrite raw_process and raw_move is no longer needed

--- raw_process.py
+++ raw_process.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
-# raw_process.py
+# raw_read.py
 # diphia@2020
-# This script is used to process the raw_inbox 
+# This script is used to process the raw_inbox file and reorganize tasks to deeper files
 
 import os
+from datetime import date
 
 CONTEXT_LOC = os.environ['HOME']+'/.vigtd_context/'
 
 raw_inbox = CONTEXT_LOC + 'raw_inbox'
 todo_list = CONTEXT_LOC + 'todo_list.csv'
+done_list = CONTEXT_LOC + 'done_list.csv'
+
+class task(object):
+    def __init__(self, name, tag, ddl):
+        self.name = name
+        self.tag = tag
+        self.ddl = ddl
+    def get_name(self):
+        return self.name
+    def get_tag(self):
+        return self.tag
+    def get_ddl(self):
+        return self.ddl
 
 def call_vim_single_line(prompt):
     with open('/tmp/raw_process_call_vim_single_line','w') as f:
@@ -19,35 +33,72 @@ def call_vim_single_line(prompt):
         line = f.read().split('\n')[0]
         content = line.split(':')[1].strip()
     return content
-    
+
 def process_line(line):
-    print('Task: ',end='')
-    print(line)
-    print('Is this easy to complete ? If so, complete it now and input [done] ; or input [no]: ',end = '')
+    task_name = line.strip()
+    print('Task: ',end = '')
+    print(task_name)
+    print('Is this easy to complete (<2min) ? If so, complete it now and input [done] ; or input [no]: ',end = '')
     while(True):
         selected = input()
         if(selected == 'done'):
-            processed_line = 'DONE' + ',' + line
+            task_tag = 'done'
             break
         elif(selected == 'no'):
-            date = call_vim_single_line('Plan to complete before')
-            processed_line = 'TODO|' + date + ',' + line
+            task_tag = 'todo'
             break
         else:
-            print('input error, please input [done] or [no]:')
-    return processed_line
+            print('input error, please input [done] or [no]: ',end = '')
+    if(task_tag == 'todo'):
+        task_ddl = call_vim_single_line('Plan to complete before')
+    else:
+        task_ddl = ''
+    return task_name,task_tag,task_ddl
 
-def reorgnize():
-    processed_lines = []
+def raw_inbox_read():
+    task_list_todo = []
+    task_list_done = []
     with open(raw_inbox,'r') as f:
-        lines = f.read().split('\n')[:-1]
-        for l in lines:
-            processed_lines.append(process_line(l))
-    print(processed_lines)
-    with open(raw_inbox,'w') as f:
-        for l in processed_lines:
-            f.write(l)
-            f.write('\n')
+        for line in f:
+            task_name,task_tag,task_ddl = process_line(line)
+            temp_task = task(task_name,task_tag,task_ddl)
+            if(temp_task.get_tag() == 'todo'):
+                task_list_todo.append(temp_task)
+            elif(temp_task.get_tag() == 'done'):
+                task_list_done.append(temp_task)
+    return task_list_done,task_list_todo
+
+def write_to_todo(task_list_todo):
+    try:
+        f = open(todo_list,'a')
+        for task in task_list_todo:
+            to_write = '\"' + task.get_name() + '\",\"' + task.get_ddl() + '\"\n'
+            f.write(to_write)
+    finally:
+        if(f):
+            f.close()
+
+def write_to_done(task_list_done):
+    try:
+        f = open(done_list,'a')
+        for task in task_list_done:
+            to_write = '\"' + task.get_name() + '\",\"\",\"' + date.today().strftime("%Y-%m-%d") + '\"\n'
+            f.write(to_write)
+    finally:
+        if(f):
+            f.close()
+
+def clear_file(file):
+    command = 'cat ' + file + ' >> ' + file + '.bak'
+    os.system(command)
+    with open(file,'w') as f:
+        f.truncate()
+
+def reorganize():
+    task_list_done,task_list_todo = raw_inbox_read()
+    write_to_todo(task_list_todo)
+    write_to_done(task_list_done)
+    clear_file(raw_inbox)
 
 if __name__=="__main__":
-    reorgnize()
+    reorganize()
